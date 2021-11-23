41f5d5f72f60d6f8fe947e5a849819181da9ff78
Barry <barrym07@gmail.com>
2021-11-23 14:30:41 -0700

    seed data created

--- migrations/20211122223511_create_posts.js
+++ migrations/20211122223511_create_posts.js
@@ -3,8 +3,8 @@ exports.up = function (knex) {
   return knex.schema.createTable('posts', function (table) {
     table.increments('id').primary();
     table.string('title').notNullable();
-    table.string('body').notNullable();
-    table.date('created_at').notNullable();
+    table.text('body').notNullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
     table.string('user_name').notNullable().references('username').inTable('users');
   });
 };
---
+++ seeds/initial_posts.js
@@ -0,0 +1,45 @@
+
+exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  return knex('posts').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('posts').insert([
+        {
+          id: 1,
+          title: "pretium et, rutrum non, hendrerit id,",
+          body: "Morbi non sapien molestie orci tincidunt adipiscing. Mauris molestie pharetra nibh. Aliquam ornare, libero at auctor ullamcorper, nisl arcu iaculis enim, sit amet ornare lectus justo eu arcu. Morbi sit amet massa. Quisque porttitor eros nec tellus. Nunc lectus pede, ultrices a, auctor non, feugiat nec, diam. Duis mi enim, condimentum eget, volutpat ornare, facilisis eget, ipsum. Donec sollicitudin adipiscing ligula. Aenean gravida nunc sed pede. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin vel arcu eu odio tristique pharetra. Quisque ac libero nec ligula consectetuer rhoncus. Nullam velit dui, semper et, lacinia vitae, sodales at, velit. Pellentesque ultricies dignissim lacus. Aliquam rutrum lorem ac risus. Morbi metus. Vivamus euismod urna. Nullam lobortis quam a felis ullamcorper viverra. Maecenas iaculis",
+          created_at: "2022-03-12 07:09:29",
+          user_name: "Yael"
+        },
+        {
+          id: 2,
+          title: "et, magna. Praesent interdum ligula",
+          body: "Curabitur egestas nunc sed libero. Proin sed turpis nec mauris blandit mattis. Cras eget nisi dictum augue malesuada malesuada. Integer id magna et ipsum cursus vestibulum. Mauris magna. Duis dignissim tempor arcu. Vestibulum ut eros non enim commodo hendrerit. Donec porttitor tellus non magna. Nam ligula elit, pretium et, rutrum non, hendrerit id, ante. Nunc mauris sapien, cursus in, hendrerit consectetuer, cursus et, magna. Praesent interdum ligula eu enim. Etiam imperdiet dictum magna. Ut tincidunt orci quis lectus. Nullam suscipit, est ac facilisis facilisis, magna tellus faucibus leo, in lobortis tellus justo sit",
+          created_at: "2022-07-04 00:38:03",
+          user_name: "Elton"
+        },
+        {
+          id: 3,
+          title: "diam. Duis mi enim,",
+          body: "Ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae Donec tincidunt. Donec vitae erat vel pede blandit congue. In scelerisque scelerisque dui. Suspendisse ac metus vitae velit egestas lacinia. Sed congue, elit sed consequat auctor, nunc nulla vulputate dui, nec tempus mauris erat eget ipsum. Suspendisse sagittis. Nullam vitae diam. Proin dolor. Nulla semper tellus id nunc interdum feugiat. Sed nec metus facilisis lorem tristique aliquet. Phasellus fermentum convallis ligula. Donec luctus aliquet odio. Etiam ligula tortor, dictum",
+          created_at: "2022-02-20 07:15:27",
+          user_name: "Valentine"
+        },
+        {
+          id: 4,
+          title: "eget, dictum",
+          body: "Arcu. Curabitur ut odio vel est tempor bibendum. Donec felis orci, adipiscing non, luctus sit amet, faucibus ut, nulla. Cras eu tellus eu augue porttitor interdum. Sed auctor odio a purus. Duis elementum, dui quis accumsan convallis, ante lectus convallis est, vitae sodales nisi magna sed dui. Fusce aliquam, enim nec tempus scelerisque, lorem ipsum sodales purus, in molestie tortor nibh sit amet orci. Ut sagittis lobortis mauris. Suspendisse aliquet molestie tellus. Aenean egestas hendrerit neque. In ornare sagittis felis. Donec tempor, est ac mattis semper, dui lectus rutrum urna, nec luctus felis purus ac",
+          created_at: "2021-03-06 19:42:05",
+          user_name: "Emi"
+        },
+        {
+          id: 5,
+          title: "id, erat. Etiam",
+          body: "Scelerisque neque. Nullam nisl. Maecenas malesuada fringilla est. Mauris eu turpis. Nulla aliquet. Proin velit. Sed malesuada augue ut lacus. Nulla tincidunt, neque vitae semper egestas, urna justo faucibus lectus, a sollicitudin orci sem eget massa. Suspendisse eleifend. Cras sed leo. Cras vehicula aliquet libero. Integer in magna. Phasellus dolor elit, pellentesque a, facilisis non, bibendum sed, est. Nunc laoreet lectus quis massa. Mauris vestibulum, neque sed dictum eleifend, nunc risus varius orci, in consequat enim diam vel arcu. Curabitur ut odio vel est tempor bibendum. Donec felis orci, adipiscing non, luctus sit amet, faucibus ut, nulla. Cras eu tellus eu augue porttitor interdum. Sed auctor odio a purus. Duis elementum, dui quis accumsan convallis, ante lectus convallis est, vitae sodales nisi magna sed dui. Fusce aliquam, enim nec tempus scelerisque,",
+          created_at: "2021-12-30 07:30:07",
+          user_name: "Sophia"
+        }
+      ]);
+    });
+};
